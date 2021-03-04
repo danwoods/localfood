86ef231cf7036a1492c3321d5eafbc1ec38c2782
Dan Woodson <woodson.dan@gmail.com>
2021-03-04 10:49:02 -0700

    Building static pages and sitemap (#19)
    * Building static pages and sitemap
    
    * removing whitespace at start/end of file

--- pages/[restaurant-id]/index.js
+++ pages/[restaurant-id]/index.js
@@ -1,1 +1,17 @@
+/** @file Individual restaurant pages */
+import Restaurants from '../../restaurants.json'
+
 export default () => <div>{'hit'}</div>
+
+export const getStaticPaths = (props) => {
+  const paths = Restaurants.map((r) => ({ params: { 'restaurant-id': r.id } }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = ({ params }) => {
+  return { props: { 'restaurant-id': params['restaurant-id'] } }
+}
